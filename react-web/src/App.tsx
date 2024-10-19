2e5ba1e56fe5631cd3d0ce7fce5678838d93cd84
Ptolemy Henson <ptolemy2002@gmail.com>
2024-10-19 15:36:20 -0400

    Another adjustment to help with readability again

--- react-web/src/App.tsx
+++ react-web/src/App.tsx
@@ -1,31 +1,33 @@
 import { useCallback, useState } from 'react';
 import { Row } from 'react-bootstrap';
-import { BSMediaQuery, useBreakpointQuery } from "@ptolemy2002/react-bs-media-queries";
+import { useBreakpointQuery } from "@ptolemy2002/react-bs-media-queries";
 import Sidebar from 'src/components/Sidebar';
 import ConversationContainer from 'src/components/ConversationContainer';
 import Header from 'src/components/Header';
 
 export default function App() {
     const [showSidebar, setShowSidebar] = useState(false);
-    const breakpointQuery = useBreakpointQuery("md", "min");
+    const isMD = useBreakpointQuery("md", "min");
+    const isXXL = useBreakpointQuery("xxl", "min");
 
     const toggleSidebar = useCallback(() => {
         setShowSidebar((v) => !v);
     }, []);
+
+    // If sidebar remains null, it will not be rendered
+    let sidebar = null;
+    if (isXXL) {
+        sidebar = <Sidebar colSize={1} />;
+    } else if (showSidebar) {
+        sidebar = <Sidebar colSize={isMD ? 3 : 12} />;
+    }
 
     return (
         <>
             <Header onMenuClick={toggleSidebar} />
 
             <Row as="main">
-                <BSMediaQuery breakpoint='xxl' comparison='min'>
-                    <Sidebar colSize={1} />
-                </BSMediaQuery>
-                
-                <BSMediaQuery breakpoint='xl' comparison='max'>
-                    {showSidebar && <Sidebar colSize={breakpointQuery ? 3 : 12} />}
-                </BSMediaQuery>
-
+                {sidebar}
                 <ConversationContainer />
             </Row>
         </>
