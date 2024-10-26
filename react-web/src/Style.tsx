870885f03f395c80acf126e5fbb94414fc310783
Ptolemy Henson <ptolemy2002@gmail.com>
2024-10-25 21:53:57 -0400

    Reimplemented each of the LESS functions with styled-components and removed the original LESS files.

--- react-web/src/Style.tsx
+++ react-web/src/Style.tsx
@@ -6,6 +6,20 @@ import { css, Interpolation } from "styled-components";
 // exist.
 export type RequiredCSSProperties = Required<_CSSProperties>;
 
+export function centerHorizontal() {
+    return css`
+        margin-left: auto;
+        margin-right: auto;
+    `;
+}
+
+export function centerVertical() {
+    return css`
+        margin-top: auto;
+        margin-bottom: auto;
+    `;
+}
+
 export function alignLeft() {
     return css`
         margin-right: auto;
@@ -18,10 +32,15 @@ export function alignRight() {
     `;
 }
 
-export function centerVertical() {
+export function alignTop() {
+    return css`
+        margin-bottom: auto;
+    `;
+}
+
+export function alignBottom() {
     return css`
         margin-top: auto;
-        margin-bottom: auto;
     `;
 }
 
@@ -53,36 +72,67 @@ export function marginY(y: RequiredCSSProperties["marginTop"]) {
     `;
 }
 
-export function bsBreakpointMin(breakpoint: Breakpoint, content: Interpolation<object>) {
+export function allLinkPseudo(content: Interpolation<object>) {
     return css`
-        @media (min-width: ${breakpointMap.get(breakpoint)}px) {
+        &:visited,
+        &:hover,
+        &:active {
             ${content}
         }
     `;
+}
 
+export function minWidth(width: RequiredCSSProperties["width"], content: Interpolation<object>) {
+    return css`
+        @media (min-width: ${width}) {
+            ${content}
+        }
+    `;
+}
+
+export function maxWidth(width: RequiredCSSProperties["width"], content: Interpolation<object>) {
+    return css`
+        @media (max-width: ${width}) {
+            ${content}
+        }
+    `;
+}
+
+export function widthRange(
+    minWidth: RequiredCSSProperties["width"],
+    maxWidth: RequiredCSSProperties["width"],
+    content: Interpolation<object>
+) {
+    return css`
+        @media (min-width: ${minWidth}) and (max-width: ${maxWidth}) {
+            ${content}
+        }
+    `;
+}
+
+export function bsBreakpointMin(breakpoint: Breakpoint, content: Interpolation<object>) {
+    return minWidth(breakpointMap.get(breakpoint) + "px", content);
 }
 
 export function bsBreakpointMax(breakpoint: Breakpoint, content: Interpolation<object>) {
     const breakpointIndex = breakpointToIndex(breakpoint);
     const isLastBreakpoint = breakpointIndex === breakpoints.length - 1;
-    return css`
-        @media (max-width: ${isLastBreakpoint ? "100%" : breakpointMap.get(breakpoints[breakpointIndex + 1])! - 1 + "px"}) {
-            ${content}
-        }
-    `;
 
+    return maxWidth(
+        isLastBreakpoint ? 
+            "100%"
+        : 
+            breakpointMap.get(breakpoints[breakpointIndex + 1])! - 1 + "px",
+        content
+    );
 }
 
 export function bsBreakpointSame(breakpoint: Breakpoint, content: Interpolation<object>) {
     const breakpointIndex = breakpointToIndex(breakpoint);
-    return css`
-        @media (
-            min-width: ${breakpointMap.get(breakpoint)}px
-        ) and (
-            max-width: ${breakpointIndex === breakpoints.length - 1 ? "100%" : breakpointMap.get(breakpoints[breakpointIndex + 1])! - 1 + "px"}
-        ) {
-            ${content}
-        }
-    `;
 
+    return widthRange(
+        breakpointMap.get(breakpoint) + "px",
+        breakpointIndex === breakpoints.length - 1 ? "100%" : breakpointMap.get(breakpoints[breakpointIndex + 1])! - 1 + "px",
+        content
+    );
 }
