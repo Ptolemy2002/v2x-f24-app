b0e2c1edc8e4f9d8a9f71b502bce494bd60fc7e1
Ptolemy Henson <ptolemy2002@gmail.com>
2024-11-04 09:35:18 -0500

    Beginnings of a new method of embedding icons

--- react-web/src/components/icons/MenuIcon.tsx
+++ react-web/src/components/icons/MenuIcon.tsx
@@ -1,3 +1,26 @@
-export default function MenuIcon() {
-    return <i className="bi bi-list" />
-}
+import SVG from 'react-inlinesvg';
+import { RequiredCSSProperties, WithCSSProp } from 'src/Style';
+import styled from 'styled-components';
+
+export type MenuIconStyleAttributes = {
+    $fill?: RequiredCSSProperties['fill'] | null;
+    $width?: RequiredCSSProperties['width'];
+    $height?: RequiredCSSProperties['height'];
+};
+
+export default styled(SVG).attrs<WithCSSProp<MenuIconStyleAttributes>>(
+    (props) => ({
+        src: '/icons/menu.svg',
+        $fill: props.$fill ?? null,
+        $width: props.$width ?? '24px',
+        $height: props.$height ?? '24px',
+    })
+)`
+    width: ${({$width}) => $width};
+    height: ${({$height}) => $height};
+    margin: 0 8px;
+
+    > path {
+        stroke: ${({$fill, theme}) => $fill ?? theme.textColor};
+    }
+`;
--- react-web/src/main.tsx
+++ react-web/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from 'src/App';
 import { ThemeProvider, createGlobalStyle } from 'styled-components';
+import CacheProvider from "react-inlinesvg/provider";
 
 const GlobalStyle = createGlobalStyle`
     :root {
@@ -38,25 +39,27 @@ const GlobalStyle = createGlobalStyle`
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={{
-        backgroundColor: "#343541",
-        headerBackgroundColor: "#202123",
-        textColor: "white",
-        senderColor: "#19c37d",
-        senderTextColor: "white",
-        recepientColor: "#40414F",
-        timestampColor: "#bbb",
-        inputColor: "#555",
-        inputTextColor: "white",
-        recepientTextColor: "white",
-        audioPlayerProgressColor: "#888",
-        audioPlayerBackgroundColor: "#343541",
-        borderThickness: "1px",
-        borderColor: "white",
-        borderStyle: "solid",
-    }}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <CacheProvider>
+      <ThemeProvider theme={{
+          backgroundColor: "#343541",
+          headerBackgroundColor: "#202123",
+          textColor: "white",
+          senderColor: "#19c37d",
+          senderTextColor: "white",
+          recepientColor: "#40414F",
+          timestampColor: "#bbb",
+          inputColor: "#555",
+          inputTextColor: "white",
+          recepientTextColor: "white",
+          audioPlayerProgressColor: "#888",
+          audioPlayerBackgroundColor: "#343541",
+          borderThickness: "1px",
+          borderColor: "white",
+          borderStyle: "solid",
+      }}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </CacheProvider>
   </StrictMode>,
 );
