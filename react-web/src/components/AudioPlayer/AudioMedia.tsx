fd9cadd1e68816681d619c0badcdac2382ff50b2
Ptolemy Henson <ptolemy2002@gmail.com>
2024-11-08 09:15:35 -0500

    Better SpeechBubbleTyping improvements

--- react-web/src/components/AudioPlayer/AudioMedia.tsx
+++ react-web/src/components/AudioPlayer/AudioMedia.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, useRef } from 'react';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
 import { AudioMediaProps } from './Types';
 import { isMobile } from 'react-device-detect';
 import { useMountEffect } from '@ptolemy2002/react-mount-effects';
@@ -8,12 +8,19 @@ export default Object.assign(
         HTMLAudioElement, AudioMediaProps
     >(({src, onCanPlay, onTimeUpdate, mobileTimeUpdateInterval=500, ...props}, ref) => {
         const intervalRef = useRef<number | null>(null);
+        const audioRef = useRef<HTMLAudioElement | null>(null);
         const timeUpdateListener = () => {
             onTimeUpdate?.();
         };
 
+        // Forward the value of audioRef to the parent component.
+        useImperativeHandle(ref, () => audioRef.current!, [audioRef.current]);
+
         useMountEffect(() => {
             if (isMobile) {
+                // Set the interval for mobile devices, as onTimeUpdate is not supported
+                // in the same way as it is on desktop. We need to catch updated time values
+                // manually.
                 intervalRef.current = window.setInterval(() => {
                     timeUpdateListener();
                 }, mobileTimeUpdateInterval);
@@ -22,7 +29,7 @@ export default Object.assign(
 
         return (
             <audio
-                ref={ref}
+                ref={audioRef}
                 onTimeUpdate={!isMobile ? timeUpdateListener : undefined}
                 onSeeked={isMobile ? timeUpdateListener : undefined}
 
--- react-web/src/components/SpeechBubble/TypingStyled.ts
+++ react-web/src/components/SpeechBubble/TypingStyled.ts
@@ -1,3 +1,4 @@
+import { math } from "polished";
 import { speechBubbleBaseStyle } from "./Other";
 import { SpeechBubbleTypingStyleAttributes } from "./Types";
 import Typing from "./Typing";
@@ -12,14 +13,22 @@ export default Object.assign(
             $fontSize: props.$fontSize ?? "3rem",
             // ch is based on the width of the 0 character in the font. In monospace fonts, this is the same for all characters.
             // In non-monospace fonts, it's good enough for a rough estimate.
-            $minWidth: props.$minWidth ?? ((props.maxDots ?? 3) - 1) + "ch",
+            $minWidth: props.$minWidth ?? ((props.maxDots ?? 3) / 2.5) + "ch",
             $css: props.$css ?? null,
         })
     )`
         ${({$maxWidth, $padding, $borderRadius}) => speechBubbleBaseStyle($maxWidth!, $borderRadius!, $padding!)}
 
-        font-size: ${({$fontSize}) => $fontSize};
-        min-width: ${({$minWidth}) => $minWidth};
+        > .speech-bubble-content {
+            padding-bottom: 0;
+
+            > .typing-indicator {
+                display: inline-block;
+                font-size: ${({$fontSize}) => $fontSize};
+                line-height: ${({$fontSize}) => math(`${$fontSize} / 2`)};
+                min-width: ${({$minWidth}) => $minWidth};   
+            }
+        }
 
         ${({$css}) => $css}
     `,
