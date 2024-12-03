41e903fc9d2b391046bca48f014748f9b6ca0b9f
Ptolemy2002 <ptolemy2002@gmail.com>
2024-12-03 11:01:28 -0500

    Updated NamedTheme to include theme metadata and allow accessing by index.

--- react-web/src/NamedTheme.tsx
+++ react-web/src/NamedTheme.tsx
@@ -2,59 +2,81 @@ import { ComponentProps, createContext, useCallback, useContext, useState } from
 import { MaybeTransformer } from "@ptolemy2002/ts-utils";
 import { ThemeProvider, useTheme } from "styled-components";
 import isCallable from "is-callable";
+import { DefaultTheme } from "styled-components/dist/types";
 
 export type ThemeProviderProps = ComponentProps<typeof ThemeProvider>;
 
-export const NamedThemes: Record<string, ThemeProviderProps["theme"]> = {
-    dark: {
-        backgroundColor: "#343541",
-        headerBackgroundColor: "#202123",
-        textColor: "white",
-        activeTextColor: "#19c37d",
-        senderColor: "#19c37d",
-        senderTextColor: "white",
-        recepientColor: "#40414F",
-        timestampColor: "#bbb",
-        inputColor: "#555",
-        inputTextColor: "white",
-        recepientTextColor: "white",
-        audioPlayerProgressColor: "#888",
-        audioPlayerBackgroundColor: "#343541",
-        dangerIconColor: "white",
-        dangerIconBackgroundColor: "red",
-        borderThickness: "1px",
-        borderColor: "white",
-        borderStyle: "solid"
-    }
+export type NamedTheme = {
+    id: string,
+    displayName: string;
+    value: ThemeProviderProps["theme"];
 };
 
-export type SetNamedThemeCallback = (theme: MaybeTransformer<string, [string]>) => void;
+export const NamedThemes: NamedTheme[] = [
+    {
+        id: "dark",
+        displayName: "Dark",
+        value: {
+            backgroundColor: "#343541",
+            headerBackgroundColor: "#202123",
+            textColor: "white",
+            activeTextColor: "#19c37d",
+            senderColor: "#19c37d",
+            senderTextColor: "white",
+            recepientColor: "#40414F",
+            timestampColor: "#bbb",
+            inputColor: "#555",
+            inputTextColor: "white",
+            recepientTextColor: "white",
+            audioPlayerProgressColor: "#888",
+            audioPlayerBackgroundColor: "#343541",
+            dangerIconColor: "white",
+            dangerIconBackgroundColor: "red",
+            borderThickness: "1px",
+            borderColor: "white",
+            borderStyle: "solid"
+        }
+    }
+];
+
+export type SetNamedThemeCallback = (theme: MaybeTransformer<string | number, [string]>) => void;
 const NamedThemeSetterContext = createContext<SetNamedThemeCallback | undefined>(undefined);
 
-export function verifyTheme(theme: string) {
-    if (!NamedThemes[theme]) {
-        throw new Error(`Theme ${theme} not found`);
-    }
+export function findThemeByValue(value: DefaultTheme) {
+    return NamedThemes.find((theme) => theme.value === value) ?? null;
 }
 
-export type NamedThemeProviderProps = Omit<ThemeProviderProps, "theme"> & {initial: string};
-export function NamedThemeProvider({initial, ...props}: NamedThemeProviderProps) {
-    const [currentTheme, _setCurrentTheme] = useState<string>(() => {
-        verifyTheme(initial);
-        return initial;
-    });
+export function findThemeById(id: string) {
+    return NamedThemes.find((theme) => theme.id === id) ?? null;
+}
+
+export function verifyTheme(theme: string | number) {
+    if (typeof theme === "number") {
+        if (!NamedThemes[theme]) {
+            throw new Error(`Theme with index ${theme} not found`);
+        }
+        return NamedThemes[theme].id;
+    } else if (!findThemeById(theme)) {
+        throw new Error(`Theme ${theme} not found`);
+    }
+
+    return theme;
+}
+
+export type NamedThemeProviderProps = Omit<ThemeProviderProps, "theme"> & {initial: string | number};
+export function NamedThemeProvider({initial=0, ...props}: NamedThemeProviderProps) {
+    const [currentTheme, _setCurrentTheme] = useState<string>(() => verifyTheme(initial));
 
     const setCurrentTheme = useCallback<SetNamedThemeCallback>((theme) => {
         _setCurrentTheme((prev) => {
             const next = isCallable(theme) ? theme(prev) : theme;
-            verifyTheme(next);
-            return next;
+            return verifyTheme(next);
         });
     }, [_setCurrentTheme]);
 
     return (
         <NamedThemeSetterContext.Provider value={setCurrentTheme}>
-            <ThemeProvider theme={NamedThemes[currentTheme]} {...props} />
+            <ThemeProvider theme={findThemeById(currentTheme)!.value} {...props} />
         </NamedThemeSetterContext.Provider>
     );
 }
@@ -67,5 +89,5 @@ export function useNamedTheme() {
         throw new Error("useNamedTheme must be used within a NamedThemeProvider");
     }
 
-    return [theme, setTheme] as const;
+    return [findThemeByValue(theme)!, setTheme] as const;
 }
