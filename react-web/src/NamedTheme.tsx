3648937849fe2963dfd7188168d682e88c97ab1f
Ptolemy2002 <ptolemy2002@gmail.com>
2025-01-21 16:02:31 -0500

    Enhanced NamedTheme capabilities

--- react-web/src/NamedTheme.tsx
+++ react-web/src/NamedTheme.tsx
@@ -1,20 +1,31 @@
-import { ComponentProps, createContext, useCallback, useContext, useState } from "react";
+import { ComponentProps, createContext, useCallback, useContext } from "react";
 import { MaybeTransformer } from "@ptolemy2002/ts-utils";
 import { ThemeProvider, useTheme } from "styled-components";
 import isCallable from "is-callable";
 import { DefaultTheme } from "styled-components/dist/types";
+import { wrapNumber } from "@ptolemy2002/js-math-utils";
+import { usePersistentState } from "@ptolemy2002/react-utils";
 
 export type ThemeProviderProps = ComponentProps<typeof ThemeProvider>;
 
-export type NamedTheme = {
-    id: string,
+export type NamedThemeReservedIds = "detect";
+
+export type NamedTheme= {
+    id: string;
     displayName: string;
     value: ThemeProviderProps["theme"];
+    onSwitch?: (byIndex: boolean) => void;
 };
 
+export function createNamedTheme<T extends string>(
+    id: T extends NamedThemeReservedIds ? never : T,
+    theme: Omit<NamedTheme, "id">
+): NamedTheme & {id: T} {
+    return {id, ...theme};
+}
+
 export const NamedThemes: NamedTheme[] = [
-    {
-        id: "dark",
+    createNamedTheme("dark", {
         displayName: "Dark",
         value: {
             backgroundColor: "#343541",
@@ -61,11 +72,15 @@ export const NamedThemes: NamedTheme[] = [
                 style: "solid"
             }
         }
-    }
+    })
 ];
 
 export type SetNamedThemeCallback = (theme: MaybeTransformer<string | number, [string]>) => void;
-const NamedThemeSetterContext = createContext<SetNamedThemeCallback | undefined>(undefined);
+const NamedThemeSetterContext = createContext<{
+    setTheme: SetNamedThemeCallback,
+    nextTheme: () => void,
+    prevTheme: () => void
+} | undefined>(undefined);
 
 export function findThemeByValue(value: DefaultTheme) {
     return NamedThemes.find((theme) => theme.value === value) ?? null;
@@ -75,12 +90,18 @@ export function findThemeById(id: string) {
     return NamedThemes.find((theme) => theme.id === id) ?? null;
 }
 
+export function findThemeIndexById(id: string) {
+    return NamedThemes.findIndex((theme) => theme.id === id);
+}
+
 export function verifyTheme(theme: string | number) {
     if (typeof theme === "number") {
         if (!NamedThemes[theme]) {
             throw new Error(`Theme with index ${theme} not found`);
         }
         return NamedThemes[theme].id;
+    } else if (theme === "detect") {
+        return "detect";
     } else if (!findThemeById(theme)) {
         throw new Error(`Theme ${theme} not found`);
     }
@@ -88,19 +109,56 @@ export function verifyTheme(theme: string | number) {
     return theme;
 }
 
-export type NamedThemeProviderProps = Omit<ThemeProviderProps, "theme"> & {initial: string | number};
-export function NamedThemeProvider({initial=0, ...props}: NamedThemeProviderProps) {
-    const [currentTheme, _setCurrentTheme] = useState<string>(() => verifyTheme(initial));
+export function interpretTheme(theme: string | number) {
+    if (theme === "detect") {
+        return window.matchMedia?.("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+    }
 
-    const setCurrentTheme = useCallback<SetNamedThemeCallback>((theme) => {
+    return verifyTheme(theme);
+}
+
+export type NamedThemeProviderProps = Omit<ThemeProviderProps, "theme"> & {initial?: string | number};
+export function NamedThemeProvider({initial="detect", ...props}: NamedThemeProviderProps) {
+    const [_currentTheme, _setCurrentTheme] = usePersistentState<string>(
+        "theme", verifyTheme(initial)
+    );
+
+    const currentTheme = interpretTheme(_currentTheme);
+
+    const setTheme = useCallback<SetNamedThemeCallback>((theme) => {
         _setCurrentTheme((prev) => {
             const next = isCallable(theme) ? theme(prev) : theme;
-            return verifyTheme(next);
+            if (next === prev) return prev;
+
+            if (next === "detect") {
+                return "detect";
+            } else {
+                const nextTheme = findThemeById(verifyTheme(next))!;
+                nextTheme.onSwitch?.(typeof next === "number");
+                
+                return nextTheme.id;
+            }
+        });
+    }, [_setCurrentTheme]);
+
+    const nextTheme = useCallback(() => {
+        _setCurrentTheme((prev) => {
+            prev = interpretTheme(prev);
+            const index = findThemeIndexById(prev);
+            return NamedThemes[wrapNumber(index + 1, 0, NamedThemes.length)]!.id;
+        });
+    }, [_setCurrentTheme]);
+
+    const prevTheme = useCallback(() => {
+        _setCurrentTheme((prev) => {
+            prev = interpretTheme(prev);
+            const index = findThemeIndexById(prev);
+            return NamedThemes[wrapNumber(index - 1, 0, NamedThemes.length)]!.id;
         });
     }, [_setCurrentTheme]);
 
     return (
-        <NamedThemeSetterContext.Provider value={setCurrentTheme}>
+        <NamedThemeSetterContext.Provider value={{setTheme, nextTheme, prevTheme}}>
             <ThemeProvider theme={findThemeById(currentTheme)!.value} {...props} />
         </NamedThemeSetterContext.Provider>
     );
@@ -108,11 +166,11 @@ export function NamedThemeProvider({initial=0, ...props}: NamedThemeProviderProp
 
 export function useNamedTheme() {
     const theme = useTheme();
-    const setTheme = useContext(NamedThemeSetterContext);
+    const themeSetter = useContext(NamedThemeSetterContext);
 
-    if (setTheme === undefined) {
+    if (themeSetter === undefined) {
         throw new Error("useNamedTheme must be used within a NamedThemeProvider");
     }
 
-    return [findThemeByValue(theme)!, setTheme] as const;
+    return [findThemeByValue(theme)!, themeSetter] as const;
 }
